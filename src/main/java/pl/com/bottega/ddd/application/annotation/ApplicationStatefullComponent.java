531bbacc6141053cc9251a03de37029e41d78d94
SlawekSobotka <ssobot@gmail.com>
2011-12-12 21:29:17 +0000

    TX added

--- src/main/java/pl/com/bottega/ddd/application/annotation/ApplicationStatefullComponent.java
+++ src/main/java/pl/com/bottega/ddd/application/annotation/ApplicationStatefullComponent.java
@@ -3,15 +3,24 @@
  */
 package pl.com.bottega.ddd.application.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author Slawek
  */
 @Component
+@Transactional
 @Scope(value="session", proxyMode=ScopedProxyMode.TARGET_CLASS)
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface ApplicationStatefullComponent {
 
 }
