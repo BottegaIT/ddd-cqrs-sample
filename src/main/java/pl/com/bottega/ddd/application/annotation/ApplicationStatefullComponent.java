6bc37c523e853826d40a76c43f92ff92bc3ca493
SlawekSobotka <ssobot@gmail.com>
2011-12-12 21:18:42 +0000

    bugfix: missing session scope

--- src/main/java/pl/com/bottega/ddd/application/annotation/ApplicationStatefullComponent.java
+++ src/main/java/pl/com/bottega/ddd/application/annotation/ApplicationStatefullComponent.java
@@ -3,12 +3,15 @@
  */
 package pl.com.bottega.ddd.application.annotation;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 /**
  * @author Slawek
  */
 @Component
+@Scope(value="session", proxyMode=ScopedProxyMode.TARGET_CLASS)
 public @interface ApplicationStatefullComponent {
 
 }
