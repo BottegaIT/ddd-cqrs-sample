a55277e9f3e06dd5bb999b57fea8e240d83c119f
SlawekSobotka <ssobot@gmail.com>
2011-12-12 21:29:02 +0000

    TX optimisation

--- src/main/java/pl/com/bottega/cqrs/query/annotations/Finder.java
+++ src/main/java/pl/com/bottega/cqrs/query/annotations/Finder.java
@@ -3,13 +3,22 @@
  */
 package pl.com.bottega.cqrs.query.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author Slawek
  * 
  */
 @Service
+@Transactional(readOnly=true)
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Finder {
 
 }
